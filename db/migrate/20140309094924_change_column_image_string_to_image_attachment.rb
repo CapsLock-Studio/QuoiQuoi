0919629f6476c70a60e2f2aeef03ed845db9aff7
Calvin-Huang <ts01364362@gmail.com>
2014-05-11 14:17:56 +0900

    fix migrate

--- db/migrate/20140309094924_change_column_image_string_to_image_attachment.rb
+++ db/migrate/20140309094924_change_column_image_string_to_image_attachment.rb
@@ -1,6 +1,5 @@
 class ChangeColumnImageStringToImageAttachment < ActiveRecord::Migration
   def change
     remove_column :products, :image
-    remove_column :product_custom_items, :image
   end
 end
