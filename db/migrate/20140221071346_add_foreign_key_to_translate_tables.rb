734fc549679504f9600e90fee974e42d167126a2
Calvin-Huang <ts01364362@gmail.com>
2014-05-11 14:17:00 +0900

    fix migrate

--- db/migrate/20140221071346_add_foreign_key_to_translate_tables.rb
+++ db/migrate/20140221071346_add_foreign_key_to_translate_tables.rb
@@ -1,7 +1,5 @@
 class AddForeignKeyToTranslateTables < ActiveRecord::Migration
   def change
     add_column :product_translates, :product_id, :integer
-    add_column :product_custom_type_translates, :product_custom_type_id, :integer
-    add_column :product_custom_item_translates, :product_custom_item_id, :integer
   end
 end
