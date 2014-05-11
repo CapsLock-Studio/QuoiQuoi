d96e4a824ee906fb98d83ab427ff59f31070bef1
Calvin-Huang <ts01364362@gmail.com>
2014-05-11 14:20:22 +0900

    fix migrate

--- db/migrate/20140310081341_add_refence_indexs.rb
+++ db/migrate/20140310081341_add_refence_indexs.rb
@@ -3,15 +3,6 @@ class AddRefenceIndexs < ActiveRecord::Migration
     add_index :product_translates, [:locale_id], name: 'index_product_translates_on_locale_id'
     add_index :product_translates, [:product_id], name: 'index_product_translates_on_product_id'
 
-    add_index :product_custom_type_translates, [:locale_id], name: 'index_product_custom_type_translates_on_locale_id'
-    add_index :product_custom_type_translates, [:product_custom_type_id], name: 'index_product_custom_type_translates_on_product_custom_type_id'
-
-    add_index :product_custom_items, [:product_id], name: 'index_product_custom_items_on_product_id'
-    add_index :product_custom_items, [:product_custom_type_id], name: 'index_product_custom_items_on_product_custom_type_id'
-
-    add_index :product_custom_item_translates, [:locale_id], name: 'index_product_custom_item_translates_on_locale_id'
-    add_index :product_custom_item_translates, [:product_custom_item_id], name: 'index_product_custom_item_translates_on_product_custom_item_id'
-
     add_index :orders, [:user_id], name: 'index_orders_on_user_id'
 
     add_index :ships, [:order_id], name: 'index_ships_on_order_id'
