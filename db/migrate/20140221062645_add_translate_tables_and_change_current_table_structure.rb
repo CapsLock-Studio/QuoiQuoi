326648e3a7bd5b8aee1643fc9d8f1f2cddf9391e
Calvin-Huang <ts01364362@gmail.com>
2014-05-11 14:15:47 +0900

    fix migrate

--- db/migrate/20140221062645_add_translate_tables_and_change_current_table_structure.rb
+++ db/migrate/20140221062645_add_translate_tables_and_change_current_table_structure.rb
@@ -1,7 +1,5 @@
 class AddTranslateTablesAndChangeCurrentTableStructure < ActiveRecord::Migration
   def change
     remove_column :products, :name
-    remove_column :product_custom_types, :name
-    remove_column :product_custom_items, :name
   end
 end
