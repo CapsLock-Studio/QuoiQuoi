d40e8d49c082cc871e983fcc4064d172b5a7e6bd
Calvin-Huang <ts01364362@gmail.com>
2014-05-18 08:35:48 +0900

    config files update test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ QuoiQuoi::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
-  config.assets.precompile += %w(unify.js unify.css flatty.js flatty.css)
+  config.assets.precompile += %w(unify.js unify.css flatty.js flatty.css admin.js)
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
