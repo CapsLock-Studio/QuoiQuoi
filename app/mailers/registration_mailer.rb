d04df0ded6410ab5f0aedead0bc6760d3820d126
Calvin-Huang <ts01364362@gmail.com>
2014-06-26 19:16:01 +0900

    fix translate and mailer

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -5,20 +5,20 @@ class RegistrationMailer < ActionMailer::Base
     @registration = registration
     @locale_id = locale_id
     @domain = domain
-    mail(to: (registration.user)? user.email : registration.email, subject: '[quoiquoi.tw] 課程匯款提醒通知信 (系統自動寄出請勿回覆)')
+    mail(to: (registration.user)? registration.user.email : registration.email, subject: '[quoiquoi.tw] 課程匯款提醒通知信 (系統自動寄出請勿回覆)')
   end
 
   def remind(registration, locale_id, domain)
     @registration = registration
     @locale_id = locale_id
     @domain = domain
-    mail(to: (registration.user)? user.email : registration.email, subject: '[quoiquoi.tw] 課程提醒通知信 (系統自動寄出請勿回覆)')
+    mail(to: (registration.user)? registration.user.email : registration.email, subject: '[quoiquoi.tw] 課程提醒通知信 (系統自動寄出請勿回覆)')
   end
 
   def cancel_remind(registration, locale_id, domain)
     @registration = registration
     @locale_id = locale_id
     @domain = domain
-    mail(to: (registration.user)? user.email : registration.email, subject: "[quoiquoi.tw] 課程取消通知信 #{'(請協助回復退還報名金所需資料)' if registration.payment && registration.payment.completed?}")
+    mail(to: (registration.user)? registration.user.email : registration.email, subject: "[quoiquoi.tw] 課程取消通知信 #{'(請協助回復退還報名金所需資料)' if registration.payment && registration.payment.completed?}")
   end
 end
