6f010608fc5e07fd9c157a64158368239d9d4e09
Calvin-Huang <ts01364362@gmail.com>
2014-06-26 14:44:06 +0900

    placeholder image fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,6 +10,6 @@ class Course < ActiveRecord::Base
 
   has_many :registrations
 
-  has_attached_file :image, styles: {thumb: '100x75#', small: '300x225#', medium: '500x375#', large: '1000x750#'}, default_url: '/assets/:style.gif'
+  has_attached_file :image, styles: {thumb: '100x75#', small: '300x225#', medium: '500x375#', large: '1000x750#'}, default_url: '/system/placeholder/:style.gif'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -3,6 +3,6 @@ class Material < ActiveRecord::Base
   has_many :order_custom_item_materials, dependent: :destroy
 
   accepts_nested_attributes_for :material_translates, allow_destroy: true
-  has_attached_file :image, styles: {thumb: '100x75#', medium: '500x375#', large: '1000x750#'}, default_url: '/assets/:style.gif'
+  has_attached_file :image, styles: {thumb: '100x75#', medium: '500x375#', large: '1000x750#'}, default_url: '/system/placeholder/:style.gif'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
