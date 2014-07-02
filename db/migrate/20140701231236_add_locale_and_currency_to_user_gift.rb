95eedbf49f2dc7835667db03b779ff0dbf3810f1
Calvin-Huang <ts01364362@gmail.com>
2014-07-02 10:26:17 +0900

    fix migration

--- db/migrate/20140701231236_add_locale_and_currency_to_user_gift.rb
+++
@@ -1,6 +0,0 @@
-class AddLocaleAndCurrencyToUserGift < ActiveRecord::Migration
-  def change
-    add_column :user_gifts, :currency, :string
-    add_column :user_gifts, :locale_id, :integer
-  end
-end
