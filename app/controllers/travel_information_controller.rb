e74f78819fc28b583c552e797e586eba93ba7835
Calvin-Huang <ts01364362@gmail.com>
2014-07-02 10:21:41 +0900

    modify article show

--- app/controllers/travel_information_controller.rb
+++ app/controllers/travel_information_controller.rb
@@ -1,9 +1,11 @@
 class TravelInformationController < ApplicationController
   def show
-    add_breadcrumb t('article')
+    add_breadcrumb t('home'), :root_path
+    add_breadcrumb t('tourist_attractions'), :areas_path
+
+    @areas = Area.where(locale_id: session[:locale_id]).order(:id)
     @travel_information = TravelInformation.find(params[:id])
-    @articles = TravelInformation.order(created_at: :desc).page(params[:page]).per(12)
-    add_breadcrumb '旅遊景點'
+
     add_breadcrumb @travel_information.area.name
   end
 end
