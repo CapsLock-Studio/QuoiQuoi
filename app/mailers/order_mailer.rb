1c3a2d4b6cca7136b367797b69cb6ef9655c9292
Calvin-Huang <ts01364362@gmail.com>
2014-07-02 13:13:06 +0900

    change mail content

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -19,6 +19,12 @@ class OrderMailer < ActionMailer::Base
   def re_remittance_remind(order, domain)
     I18n.locale = Locale.find(order.locale_id).lang
 
+    shipping_fee = ShippingFeeTranslate.where(shipping_fee_id: order.shipping_fee_id, locale_id: order.locale_id).first
+    @fee = shipping_fee.fee
+    if !shipping_fee.free_condition.blank? && (order.subtotal > shipping_fee.free_condition)
+      @fee = 0
+    end
+
     @order = order
     @domain = domain
     mail(to: order.user.email, subject: t('mailer.subject_for_re_remittance'))
@@ -40,6 +46,12 @@ class OrderMailer < ActionMailer::Base
 
   def delivered(order, domain)
     I18n.locale = Locale.find(order.locale_id).lang
+
+    shipping_fee = ShippingFeeTranslate.where(shipping_fee_id: order.shipping_fee_id, locale_id: order.locale_id).first
+    @fee = shipping_fee.fee
+    if !shipping_fee.free_condition.blank? && (order.subtotal > shipping_fee.free_condition)
+      @fee = 0
+    end
 
     @order = order
     @domain = domain
