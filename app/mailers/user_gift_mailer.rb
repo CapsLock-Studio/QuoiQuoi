36ec6e1c2fd7948deaf8d6b6dea7ad39d23b3e1a
Calvin-Huang <ts01364362@gmail.com>
2014-07-03 17:46:13 +0900

    add user gift card re remittance mail

--- app/mailers/user_gift_mailer.rb
+++ app/mailers/user_gift_mailer.rb
@@ -9,7 +9,10 @@ class UserGiftMailer < ActionMailer::Base
   end
 
   def re_remittance_remind(user_gift)
+    I18n.locale = Locale.find(user_gift.locale_id).lang
 
+    @user_gift = user_gift
+    mail(to: user_gift.user.email, subject: t('mailer.subject_for_re_remittance'))
   end
 
   def completed_remind(user_gift)
