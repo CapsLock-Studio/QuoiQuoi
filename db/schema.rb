b37e50a5f27f916b6ab77c981604f7d6532e8115
Calvin-Huang <ts01364362@gmail.com>
2014-07-04 14:18:23 +0900

    remove translate columns from order custom item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140704042058) do
+ActiveRecord::Schema.define(version: 20140704051659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -436,7 +436,6 @@ ActiveRecord::Schema.define(version: 20140704042058) do
     t.integer  "workday"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "price"
     t.boolean  "accept"
     t.string   "name"
     t.string   "phone"
