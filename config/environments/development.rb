229a233b8670feb06afb51df2bd0ea032b390647
Calvin-Huang <ts01364362@gmail.com>
2014-07-06 00:36:21 +0900

    remove sensitive information

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ QuoiQuoi::Application.configure do
       :port => "587",
       :domain => "gmail.com",
       :authentication => "plain",
-      :user_name => "calvin.peak@gmail.com",
-      :password => "Calvin12364362",
+      :user_name => "example@gmail.com",
+      :password => "...",
       :enable_starttls_auto => true
   }
 
