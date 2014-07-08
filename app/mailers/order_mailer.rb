9535246f001454e48351842a3f0addc65b132cea
Calvin-Huang <ts01364362@gmail.com>
2014-07-08 14:14:45 +0900

    add take task

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -44,6 +44,19 @@ class OrderMailer < ActionMailer::Base
     mail(to: order.user.email, subject: t('mailer.subject_for_remittance_order'))
   end
 
+  def remittance_remind_three_days(order)
+    I18n.locale = Locale.find(order.locale_id).lang
+
+    shipping_fee = ShippingFeeTranslate.where(shipping_fee_id: order.shipping_fee_id, locale_id: order.locale_id).first
+    @fee = shipping_fee.fee
+    if !shipping_fee.free_condition.blank? && (order.subtotal > shipping_fee.free_condition)
+      @fee = 0
+    end
+
+    @order = order
+    mail(to: order.user.email, subject: t('mailer.subject_for_remittance_order'))
+  end
+
   def delivered(order, domain)
     I18n.locale = Locale.find(order.locale_id).lang
 
--- app/mailers/user_gift_mailer.rb
+++ app/mailers/user_gift_mailer.rb
@@ -8,6 +8,13 @@ class UserGiftMailer < ActionMailer::Base
     mail(to: user_gift.user.email, subject: t('mailer.subject_for_remittance_gift'))
   end
 
+  def remittance_remind_three_days(user_gift)
+    I18n.locale = Locale.find(user_gift.locale_id).lang
+
+    @user_gift = user_gift
+    mail(to: user_gift.user.email, subject: t('mailer.subject_for_remittance_gift'))
+  end
+
   def re_remittance_remind(user_gift)
     I18n.locale = Locale.find(user_gift.locale_id).lang
 
