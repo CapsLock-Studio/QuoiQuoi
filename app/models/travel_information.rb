f18f9a422efe8c347be85de69f9637df0b8806bf
Calvin-Huang <ts01364362@gmail.com>
2014-07-08 12:00:46 +0900

    change tranvel information attach image size

--- app/models/travel_information.rb
+++ app/models/travel_information.rb
@@ -2,6 +2,6 @@ class TravelInformation < ActiveRecord::Base
   belongs_to :area
   has_many :travel_photos, dependent: :destroy
 
-  has_attached_file :image, styles: {thumb: '100x75#'}, default_url: '/system/placeholder/:style.gif'
+  has_attached_file :image, styles: {thumb: '200x150#'}, default_url: '/system/placeholder/:style.gif'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
