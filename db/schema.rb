e06a318a0558f4136612c1231266f6d481e2c636
Calvin-Huang <ts01364362@gmail.com>
2014-07-08 18:52:33 +0900

    change column properties

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708054239) do
+ActiveRecord::Schema.define(version: 20140708095136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -270,7 +270,7 @@ ActiveRecord::Schema.define(version: 20140708054239) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "description"
+    t.text     "description"
     t.float    "quota"
   end
 
