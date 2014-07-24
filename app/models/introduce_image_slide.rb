c3153b912a06e3e42c0da99e621ef467c1bce173
Calvin-Huang <ts01364362@gmail.com>
2014-07-24 13:37:29 +0900

    about us images

--- app/models/introduce_image_slide.rb
+++ app/models/introduce_image_slide.rb
@@ -1,4 +1,4 @@
 class IntroduceImageSlide < ActiveRecord::Base
-  has_attached_file :image, styles: {thumb: '480x125#', medium: '960×250#'}, default_url: '/system/placeholder/slide-:style.gif'
+  has_attached_file :image, styles: {medium: '480x125#', large: '960×250#'}, default_url: '/system/placeholder/slide-:style.gif'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
