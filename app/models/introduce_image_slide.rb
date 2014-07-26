f4fa84ac93a60310ea7ba5628ee0d67efb4067a3
Calvin-Huang <ts01364362@gmail.com>
2014-07-27 00:34:44 +0900

    change image size

--- app/models/introduce_image_slide.rb
+++ app/models/introduce_image_slide.rb
@@ -1,4 +1,4 @@
 class IntroduceImageSlide < ActiveRecord::Base
-  has_attached_file :image, styles: {medium: '480x125#', large: '960×250#'}, default_url: '/system/placeholder/intro-:style.gif'
+  has_attached_file :image, styles: {large: '960×300#'}, default_url: '/system/placeholder/intro-:style.gif'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
