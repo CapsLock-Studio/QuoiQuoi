84f2b2dda3607e46926bbe3d23f097b641f6d4fa
Calvin-Huang <ts01364362@gmail.com>
2014-10-07 15:38:31 +0900

    make all links open in blank page

--- app/models/course_translate.rb
+++ app/models/course_translate.rb
@@ -1,4 +1,8 @@
 class CourseTranslate < ActiveRecord::Base
   belongs_to :course
   belongs_to :locale
+
+  def description
+    read_attribute(:description).gsub('target=""', 'target="_blank"')
+  end
 end
--- app/models/product_translate.rb
+++ app/models/product_translate.rb
@@ -1,4 +1,8 @@
 class ProductTranslate < ActiveRecord::Base
   belongs_to :product
   belongs_to :locale
+
+  def description
+    read_attribute(:description).gsub('target=""', 'target="_blank"')
+  end
 end
