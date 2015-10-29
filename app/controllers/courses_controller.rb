4e3b24a1bde274f55005c7763f885a257c3f8e00
Calvin <ts01364362@gmail.com>
2015-10-29 17:58:47 +0900

    Improve past courses display.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -106,8 +106,59 @@ class CoursesController < ApplicationController
     set_breadcrumbs
     add_breadcrumb t('course.past')
 
-    @courses = Course.includes(:course_translate).where(course_translates: {locale_id: session[:locale_id]})
-                                                 .where('courses.time >= ? AND courses.time < ?', 12.months.ago, Time.now)
+    courses = Course.includes(:course_translate).where(course_translates: {locale_id: session[:locale_id]})
+
+    respond_to do |format|
+      format.json do
+        if params[:id] =~ /\d{4}-\d{1,2}/
+          courses = courses.where('courses.time' => Time.parse("#{params[:id]}-1")..Time.parse("#{params[:id]}-1").end_of_month)
+                           .order('courses.time DESC')
+                           .page(params[:page])
+                           .per(24)
+          render json: {
+                     items: courses.collect do |course|
+                       [
+                           {
+                               key: 'truncated_name',
+                               value: ApplicationController.helpers.truncate(course.course_translate.name, length: (session[:locale] == 'en')? 38 : 20)
+                           },
+                           {
+                               key: 'name',
+                               value: course.course_translate.name
+                           },
+                           {
+                               key: 'time',
+                               value: "#{course.time.strftime('%Y/%m/%d %H:%M')}-#{(course.time + course.length.hours).strftime('%H:%M')} (#{t('date.day_names')[course.time.strftime('%u').to_i - 1]})"
+                           },
+                           {
+                               key: 'teacher',
+                               value: course.course_translate.teacher
+                           },
+                           {
+                               key: 'url',
+                               value: course_path(course, month: course.time.strftime('%m'))
+                           },
+                           {
+                               key: 'image',
+                               value: course.image.url(:small)
+                           },
+                           {
+                               key: 'status',
+                               value: course.status
+                           }
+                       ]
+                     end,
+                     nextPage: (courses.total_pages > courses.current_page)? "/courses/past/#{params[:id]}.json?page=#{((params[:page] || 1).to_i + 1)}" : nil
+                 }
+        else
+          render json: ''
+        end
+      end
+      format.html do
+        @courses = courses.where('courses.time >= ? AND courses.time < ?', 12.months.ago, Time.now)
+                          .order('courses.time DESC')
+      end
+    end
 
     @website_title = "#{t('course.past')} | #{@website_title}"
     @meta_og_title = t('course.past')
@@ -117,7 +168,7 @@ class CoursesController < ApplicationController
   # access able from another controller
   def set_months
     @months = []
-    month_first = (Time.now.month - 1 <= 0)? 12 : Time.now.month - 1
+    month_first = (Time.now.month <= 0)? 12 : Time.now.month
     6.times do |index|
       @months << ((index + month_first > 12)? month_first + index - 12 : index + month_first)
     end
@@ -157,7 +208,7 @@ class CoursesController < ApplicationController
       # not show any past courses
       courses = Course.includes(:course_translate, :registrations)
                       .where(course_translates: {locale_id: session[:locale_id]}, visible: true)
-                      .where('courses.time > ?', Time.now)
+                      .where('courses.time > ?', Time.now.beginning_of_month)
                       .where.not(course_translates: {name: '', description: '', price: nil})
 
       if !month.nil?
@@ -167,7 +218,8 @@ class CoursesController < ApplicationController
       else
         # show now to two months after
         # @courses = @courses.where('time <= ?', Time.now + 2.months)
-        courses.order('courses.id DESC').page(page).per(24)
+        courses.where('courses.time > ?', Time.now)
+               .order('courses.id DESC').page(page).per(24)
       end
     end
 end
