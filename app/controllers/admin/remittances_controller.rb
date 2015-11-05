5732722e929603e89d86332b8524bfa766d77fff
Calvin <ts01364362@gmail.com>
2015-11-05 18:50:19 +0900

    Remittance information controller update. Use redis to restore data.

--- app/controllers/admin/remittances_controller.rb
+++ app/controllers/admin/remittances_controller.rb
@@ -1,61 +1,34 @@
 class Admin::RemittancesController < AdminController
   authorize_resource
 
-  before_action :set_remittance
-
   def show
   end
 
   def new
-    unless @remittance
-      @remittance = Remittance.new
-      Locale.all.each do |locale|
-        @remittance.remittance_translates.build(locale_id: locale.id)
-      end
-    end
   end
 
   def edit
-
   end
 
   def create
-    unless @remittance
-      @remittance = Remittance.new(remittance_params)
-      respond_to do |format|
-        if @remittance.save
-          format.html {redirect_to action: :show}
-        else
-          format.html {render action: :new, status: :unprocessable_entity}
-        end
-      end
-    end
   end
 
   def update
-    respond_to do |format|
-      if @remittance.update_attributes(remittance_params)
-        format.html {redirect_to action: :show}
-      else
-        format.html {render action: :edit}
-      end
+    remittance_params.each do |params|
+      $redis.set("remittance:locale:#{params[1][:locale_id]}:name", params[1][:name])
+      $redis.set("remittance:locale:#{params[1][:locale_id]}:code", params[1][:code])
+      $redis.set("remittance:locale:#{params[1][:locale_id]}:account", params[1][:account])
+      $redis.set("remittance:locale:#{params[1][:locale_id]}:bank_name", params[1][:bank_name])
+      $redis.set("remittance:locale:#{params[1][:locale_id]}:bank_address", params[1][:bank_address])
     end
+    redirect_to action: :show
   end
 
   def destroy
-
   end
 
   private
     def remittance_params
-      params.require(:remittance).permit(:id, remittance_translates_attributes: [:id, :remittance_id, :name, :account, :code,
-                                                                          :bank_name, :bank_address, :locale_id])
-    end
-    def set_remittance
-      begin
-        @remittance = Remittance.find(1)
-      rescue ActiveRecord::RecordNotFound
-        @remittance = nil
-      end
+      params.require(:remittance)
     end
 end
