b280b463410697b52e4af13ec00b0f0ee7ba5fb4
Calvin <ts01364362@gmail.com>
2015-11-11 00:52:00 +0900

    - When payment canceled, online payment reminder will not send mail to tell customer to continue paying. (There is a bug if payment was canceled but customer can still continue paying through the mail link which point to paypal store url.) - Update mail layout for showing layout correctly in Android.

--- app/jobs/online_payment_remind_job.rb
+++ app/jobs/online_payment_remind_job.rb
@@ -3,7 +3,7 @@ class OnlinePaymentRemindJob < ActiveJob::Base
 
   def perform(payment_object)
     # Do something later
-    unless payment_object.completed?
+    if !payment_object.cancel? && !payment_object.completed?
       if payment_object.is_a?(RegistrationPayment)
         RegistrationMailer.remind_to_pay(payment_object.registration_id, I18n.translate('mailer.subject.payment.online')).deliver_later
       end
