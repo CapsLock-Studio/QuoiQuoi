168c4aa0db360b9675010f25809481051380ec0c
Calvin <ts01364362@gmail.com>
2015-12-08 13:02:19 +0900

    Remove custom order form modal, use full page form.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,42 +1,29 @@
 class MessagesController < ApplicationController
-  before_action :set_message, except: [:index, :create, :new]
+  before_action :set_message, only: []
   before_action :authenticate_user!
 
   def index
-    @messages = Message.where(user_id: current_user.id).order(id: :desc).page(params[:page]).per(24)
   end
 
   def show
-
-    if @message.admin? && !@message.read?
-      unless @message.update_attribute(:read, true)
-        render json: @message.errors
-      end
-    end
   end
 
   def new
-
-    @message = Message.new
   end
 
   def create
-    respond_to do |format|
-      @message = Message.new(message_params.merge(user_id: current_user.id))
-      if @message.save
-        format.html {redirect_to action: :index}
-      else
-        format.html {render action: :new}
-      end
+    message = Message.new(sanitized_params)
+    if message.save
+      MessageMailer.remind_to_admin(message.id).deliver_later
+
+      redirect_to :back
+    else
+      render json: 'There is something wrong.'
     end
   end
 
   private
-    def message_params
-      params.require(:message).permit(:content)
-    end
-
-    def set_message
-      @message = Message.find(params[:id])
-    end
+  def sanitized_params
+    params.require(:message).permit(:content, :custom_order_id)
+  end
 end
