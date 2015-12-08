20dca3a15919e5e016b07eca81534aa03782c5b3
Calvin <ts01364362@gmail.com>
2015-12-08 18:19:48 +0900

    Past work types index show articles.

--- app/controllers/past_work_types_controller.rb
+++ app/controllers/past_work_types_controller.rb
@@ -5,5 +5,7 @@ class PastWorkTypesController < ApplicationController
 
     @past_work_types = PastWorkType.includes(:past_work_type_translate)
                                    .where(past_work_type_translates: {locale_id: session[:locale_id]}, visible: true).order(:sort)
+    @past_works = PastWork.includes(:past_work_translate)
+                          .where(past_work_translates: {locale_id: session[:locale_id]}, visible: true).order(created_at: :desc).page(params[:page]).per(12)
   end
 end
