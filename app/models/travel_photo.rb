8d78d9de45c7221691fc5872e9bbe282ddfec1e0
Calvin <calvin.peak@capslock.tw>
2016-07-09 18:03:45 +0900

    Change travel photo property name.

--- app/models/travel_photo.rb
+++ app/models/travel_photo.rb
@@ -1,6 +1,6 @@
 class TravelPhoto < ActiveRecord::Base
   belongs_to :travel_information
 
-  has_attached_file :photo, styles: {thumb: 'x80'}
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: {thumb: 'x80'}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
