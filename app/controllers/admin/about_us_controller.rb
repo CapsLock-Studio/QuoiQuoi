ca85fe382a8c8f64bbe3d102ff8d27dd62f64ef0
Calvin Huang <calvin.peak@capslock.tw>
2017-08-20 09:13:35 -0700

    Since upgrading to Rails 5, the params structure is different, thus update the param structure.

--- app/controllers/admin/about_us_controller.rb
+++ app/controllers/admin/about_us_controller.rb
@@ -10,12 +10,12 @@ class Admin::AboutUsController < AdminController
   end
 
   def update
-    about_params.each do |params|
-      $redis.set("about:locale:#{params[1][:locale_id]}:email", params[1][:email])
-      $redis.set("about:locale:#{params[1][:locale_id]}:phone", params[1][:phone])
-      $redis.set("about:locale:#{params[1][:locale_id]}:address", params[1][:address])
-      $redis.set("about:locale:#{params[1][:locale_id]}:business_hour", params[1][:business_hour])
-      $redis.set("about:locale:#{params[1][:locale_id]}:introduction", params[1][:introduction])
+    about_params.each do |_, params|
+      $redis.set("about:locale:#{params[:locale_id]}:email", params[:email])
+      $redis.set("about:locale:#{params[:locale_id]}:phone", params[:phone])
+      $redis.set("about:locale:#{params[:locale_id]}:address", params[:address])
+      $redis.set("about:locale:#{params[:locale_id]}:business_hour", params[:business_hour])
+      $redis.set("about:locale:#{params[:locale_id]}:introduction", params[:introduction])
     end
 
     redirect_to action: :show
