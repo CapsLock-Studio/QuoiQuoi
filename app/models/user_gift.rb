c2faa54925a341602e945b498468eb97ae1378f4
Calvin Huang <calvin.peak@capslock.tw>
2017-08-20 20:54:47 -0700

    Add virtual property to quick access model column.

--- app/models/user_gift.rb
+++ app/models/user_gift.rb
@@ -1,4 +1,6 @@
 class UserGift < ApplicationRecord
+  enum payment_method: {remittance: 0, paypal: 1, cvs_family: 2, cvs_ibon: 3, webatm: 4, atm: 5, alipay: 6, credit_card: 7}
+
   belongs_to :user
   belongs_to :gift
   belongs_to :order
@@ -8,4 +10,13 @@ class UserGift < ApplicationRecord
   has_many :user_gift_serials, dependent: :destroy
 
   belongs_to :used_user, class_name: 'User', foreign_key: 'used_user_id'
+
+  def paid?
+    self.payment
+  end
+
+  # Force input value convert to integer
+  def payment_method=(value)
+    write_attribute(:payment_method, (value.is_a?(Integer))? value : value.to_i)
+  end
 end
