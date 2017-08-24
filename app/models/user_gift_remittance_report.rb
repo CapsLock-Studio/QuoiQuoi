d8ec80be2b8b34f9a7ea507949a0820953c89d56
Calvin Huang <calvin.peak@capslock.tw>
2017-08-24 16:48:52 -0700

    Add remittance model for user gift.

---
+++ app/models/user_gift_remittance_report.rb
@@ -0,0 +1,23 @@
+class UserGiftRemittanceReport < ApplicationRecord
+  belongs_to :user_gift_payment
+
+  def status
+    status = 'denied'
+
+    if confirm.nil?
+      status = 'waiting'
+    elsif confirm
+      status = 'confirmed'
+    end
+
+    status
+  end
+
+  def account
+    if read_attribute(:account).blank?
+      '無摺存款'
+    else
+      read_attribute(:account)
+    end
+  end
+end
