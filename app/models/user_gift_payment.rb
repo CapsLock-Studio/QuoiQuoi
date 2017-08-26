d152d61a9e6848e9cfc9f71aa44b2b6529359085
Calvin Huang <calvin.peak@capslock.tw>
2017-08-26 02:24:44 -0700

    Generate serial after payment completed.

--- app/models/user_gift_payment.rb
+++ app/models/user_gift_payment.rb
@@ -3,11 +3,32 @@ class UserGiftPayment < ApplicationRecord
 
   has_many :user_gift_remittance_reports, dependent: :delete_all
 
+  after_save :create_user_gift_serial
+
   def remittance_reports_in_process?
     self.user_gift_remittance_reports.where(confirm: nil).size > 0
   end
 
   def has_valid_remittance_reports?
     self.user_gift_remittance_reports.size > 0
+  end
+
+  def create_user_gift_serial
+    if self.completed?
+      exists_serial_count = self.user_gift.user_gift_serials.size
+      (exists_serial_count...self.user_gift.quantity).each do
+        self.user_gift.user_gift_serials.create({ serial: get_uniqueness_random_string })
+      end
+    end
+  end
+
+  private
+  def get_uniqueness_random_string
+    uniqueness_random_string = SecureRandom.hex(20)
+    while UserGiftSerial.where(serial: uniqueness_random_string).first
+      uniqueness_random_string = SecureRandom.hex(20)
+    end
+
+    uniqueness_random_string
   end
 end
