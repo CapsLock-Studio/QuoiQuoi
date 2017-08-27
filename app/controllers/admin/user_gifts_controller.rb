5ef5d783ccdf7c661771aae71533b9cd03b7e5a4
Calvin Huang <calvin.peak@capslock.tw>
2017-08-26 18:09:55 -0700

    Administrator can create user gift card for event.

--- app/controllers/admin/user_gifts_controller.rb
+++ app/controllers/admin/user_gifts_controller.rb
@@ -2,7 +2,14 @@ class Admin::UserGiftsController < AdminController
   authorize_resource
 
   def index
-    @user_gifts = UserGift.all.order(id: :desc)
+    @event_gifts = UserGift.includes(:user_gift_payment)
+                       .where({ payment_method: UserGift.payment_methods[:event] })
+                       .order(id: :desc)
+
+    @user_gifts = UserGift.includes(:user_gift_payment)
+                      .where
+                      .not({ payment_method: UserGift.payment_methods[:event] })
+                      .order(id: :desc)
   end
 
   def check
@@ -20,5 +27,30 @@ class Admin::UserGiftsController < AdminController
 
   def show
     @user_gift = UserGift.find(params[:id])
+  end
+
+  def new
+    @user_gift = UserGift.new
+  end
+
+  def create
+    user_gift = UserGift.create(
+        {
+            gift_id: params[:gift_id],
+            quantity: params[:quantity],
+            payment_method: UserGift.payment_methods[:event],
+        }
+    )
+
+    UserGiftPayment.create(
+        {
+            user_gift_id: user_gift.id,
+            completed: true,
+            completed_time: Time.now,
+            payment_time: Time.now,
+        }
+    )
+
+    redirect_to action: :index
   end
 end
