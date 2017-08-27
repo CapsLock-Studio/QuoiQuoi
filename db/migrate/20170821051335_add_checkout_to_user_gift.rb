935f006c874d7495492af9e473630ccfd98bba38
Calvin Huang <calvin.peak@capslock.tw>
2017-08-27 09:34:53 -0700

    Fix migration.

--- db/migrate/20170821051335_add_checkout_to_user_gift.rb
+++ db/migrate/20170821051335_add_checkout_to_user_gift.rb
@@ -1,6 +1,5 @@
 class AddCheckoutToUserGift < ActiveRecord::Migration[5.0]
   def change
-    remove_column :user_gifts, :accept
     add_column :user_gifts, :checkout, :boolean
     add_column :user_gifts, :checkout_time, :datetime
   end
